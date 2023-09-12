eb8ee68ae0e31f0643c231041beb2f998ac5e50e
Bart X Tubalinal <bxtp4p@users.noreply.github.com>
2023-09-12 19:38:35 +0000

    Adding devcontainer files

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Start Minikube"
+minikube start
+
+echo "Install Tekton Pipelines"
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+echo "Install Tekton Triggers"
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+
+echo "Install Tekton Dashboard"
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
